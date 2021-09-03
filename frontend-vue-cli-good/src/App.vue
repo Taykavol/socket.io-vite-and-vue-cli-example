db76a26a3edade74cae8d2232c4e9305d78410eb
Taykavol <mvkvol@yandex.ru>
2021-09-03 14:53:25 +0300

    first commit

---
+++ frontend-vue-cli-good/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {io} from 'socket.io-client'
+// io is works!
+const socket = io("ws://localhost:4000/", {});
+socket.on("connect", () => {
+  // Works great!
+  alert(`connect ${socket.id}`)
+});
+
+socket.on("disconnect", () => {
+  console.log("disconnect");
+});
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<style>
+
+</style>
+
